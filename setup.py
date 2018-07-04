49a2066366f58508b07e2724b5b780d7c161c2f2
Kyle Hernandez <khernandez@bsd.uchicago.edu>
2018-07-04 19:34:39 +0000

    feat(update): rm old

--- setup.py
+++ setup.py
@@ -1,25 +1,21 @@
 import setuptools
 
 setuptools.setup(
-        name = "htseq-tool",
-        author = "Stuti Agrawal",
-        author_email = "stutia@uchicago.edu",
-        version = 0.1,
-        description = "rna-seq quantification tools",
-        url = "https://github.com/NCI-GDC/htseq-tool",
-        license = "Apache 2.0",
-        packages = setuptools.find_packages(),
-        classifiers = [
-            "Development Status :: 3 - Alpha",
-            "Intended Audience :: Developers",
-            "License :: OSI Approved :: Apache Software License",
-            "Programming Language :: Python",
-            "Programming Language :: Python :: 2",
-            "Programming Language :: Python :: 3",
-            ],
-        install_requires = [
-            'sqlalchemy',
-            'psycopg2'
-            ]
-    )
+    name = "htseq_tools",
+    author = "Kyle Hernandez",
+    author_email = "kmhernan@uchicago.edu",
+    version = 0.5,
+    description = "htseq-count output tools",
+    url = "https://github.com/NCI-GDC/htseq-tool",
+    license = "Apache 2.0",
+    packages = setuptools.find_packages(),
+    entry_points = {
+        'console_scripts': [
+            'htseq-tools = htseq_tools.__main__:main' 
+        ]
+    },
+    install_requires = [
+      'numpy==1.14.5'
+    ]
+)
 
