e99fb46c0e530b132cfa129ac0e1282e3ce6dcf3
Kyle Hernandez <khernandez@bsd.uchicago.edu>
2020-01-17 21:42:40 +0000

    chore(tests): add gene len

--- tests/test_gene_lengths.py
+++ tests/test_gene_lengths.py
@@ -3,7 +3,9 @@ import os
 
 from htseq_tools.tools.gene_lengths import (
     extract_gene_data,
-    extract_metadata)
+    extract_metadata,
+    load_gtf_data,
+    write_lengths)
 
 
 class TestGeneLengths(unittest.TestCase):
@@ -61,3 +63,41 @@ class TestGeneLengths(unittest.TestCase):
         finfo_string = '; '.join(finfo_parts) + '; '
         with self.assertRaises(AssertionError):
             gene_id, gene_type = extract_metadata(finfo_string)
+
+    def test_load_gtf_data(self):
+        gtf_file = os.path.join(os.path.dirname(__file__), 'etc/test.gtf')
+        gene_data, exon_data = load_gtf_data(gtf_file)
+
+        expected_gene = {'FAKE.1': 'pseudogene', 'FAKE.2': 'protein_coding'}
+        self.assertEqual(gene_data, expected_gene)
+
+        expected_exon = {
+            'FAKE.1': [(11869, 12227), (12613, 12721), (12010, 12057),
+                       (12179, 12227)],
+            'FAKE.2': [(29534, 29570)]
+        }
+        self.assertEqual(exon_data, expected_exon)
+
+    def test_write_lengths(self):
+        gtf_file = os.path.join(os.path.dirname(__file__), 'etc/test.gtf')
+        exp_file = os.path.join(os.path.dirname(__file__),
+                                'etc/test_lengths.txt')
+        out_file = os.path.join(os.path.dirname(__file__),
+                                'etc/test_lengths_out.txt')
+
+        gene_data, exon_data = load_gtf_data(gtf_file)
+        write_lengths(gene_data, exon_data, out_file)
+
+        with open(exp_file, 'rt') as fh, open(out_file, 'rt') as ofh:
+            exp = fh.read()
+            found = ofh.read()
+            self.assertEqual(exp, found)
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        out_file = os.path.join(os.path.dirname(__file__),
+                                'etc/test_lengths_out.txt')
+        if os.path.exists(out_file):
+            os.remove(out_file)
